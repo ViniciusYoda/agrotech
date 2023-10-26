c14a2bb18e4b0823a838cc8925b9ea4417f049b8
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-26 08:21:59 -0300

    Update index.js

--- src/screens/Cafe/index.js
+++ src/screens/Cafe/index.js
@@ -13,7 +13,7 @@ export default function Cafe() {
       <TopGreen text="Cafe" iconName="times" />
       <Info
         imageSource={require('../../assets/CafeInfo.jfif')}
-        name="Milho"
+        name="Café"
         pragas="Broca-do-café "
         description="O café, cientificamente conhecido como Coffea, é uma das bebidas mais populares em todo o mundo. Originário da Etiópia, o café é amplamente cultivado em várias regiões do planeta devido à sua apreciação por seu sabor e efeitos estimulantes. Além de ser uma bebida, os grãos de café também são usados na produção de diversos produtos, incluindo óleo de café e até mesmo joias feitas a partir de grãos de café."
       />
