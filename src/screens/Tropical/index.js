4b46e877b66e02d971c3ba8cfb7bc08fc7455bd2
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-26 09:55:45 -0300

    Create index.js

---
+++ src/screens/Tropical/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Info from '../../components/Info';
+import TopGreen from '../../components/TopGreen';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #D3D3D3;
+`;
+
+export default function ClimaTropical() {
+  return (
+    <Container>
+      <TopGreen text="Clima Tropical" iconName="times" />
+      <Info
+        imageSource={require('../../assets/ClimaTropical.jfif')}
+        name="Clima Tropical"
+        description="O clima tropical, conhecido por suas altas temperaturas
+e estações distintas de chuva e seca, é adequado para uma
+variedade de cultivos tropicais, incluindo frutas, legumes e grãos."
+      />
+    </Container>
+  );
+}
