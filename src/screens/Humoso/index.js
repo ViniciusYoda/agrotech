fd486c358d83573edc5e12ce3ea7b46ce0862606
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-26 10:11:39 -0300

    Create index.js

---
+++ src/screens/Humoso/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Info from '../../components/Info';
+import TopGreen from '../../components/TopGreen';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #D3D3D3;
+`;
+
+export default function SoloHumoso() {
+  return (
+    <Container>
+      <TopGreen text="Solo Humoso" iconName="times" />
+      <Info
+        imageSource={require('../../assets/SoloHumoso.jfif')}
+        name="Solo Humoso"
+        description="O solo humoso é caracterizado por alto teor de matéria orgânica,
+o que resulta em boa fertilidade e retenção de água. Esse tipo de solo é
+adequado para uma ampla variedade de culturas devido à sua qualidade."
+      />
+    </Container>
+  );
+}
