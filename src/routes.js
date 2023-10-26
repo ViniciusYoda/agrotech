9b29c123ac117d62bf3cce5649fe205316ff823a
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-26 08:33:12 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -15,32 +15,95 @@ import Milho from './screens/Milho';
 import Soja from './screens/Soja';
 import Cafe from './screens/Cafe';
 import CanaDeAcucar from './screens/CanaDeAcucar';
+import Mandioca from './screens/Mandioca';
 import Equatorial from './screens/Equatorial';
 import TodasNoticias from './screens/TodasNoticias';
 import TodasCotacao from './screens/TodasCotacao';
 
 export default function routes() {
-
   return (
     <NavigationContainer>
       <Tab.Navigator>
-      <Tab.Screen name="Inicio" component={Inicio} options={{ headerShown: false }} />
-       <Tab.Screen name="Buscar" component={Buscar} options={{ headerShown: false }} />
-      <Tab.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Tab.Screen name="Cadastrar" component={Cadastrar} options={{ headerShown: false }} />
-         
-       
-        <Tab.Screen name="Conta" component={Conta} options={{ headerShown: false }} />
-        <Tab.Screen name="Perfil" component={Perfil} options={{ headerShown: false }} />
-        <Tab.Screen name="Plantas" component={Plantas} options={{ headerShown: false }} />
-        <Tab.Screen name="Climas" component={Climas} options={{ headerShown: false }} />
-        <Tab.Screen name="Milho" component={Milho} options={{ headerShown: false }} />
-        <Tab.Screen name="Soja" component={Soja} options={{ headerShown: false }} />
-        <Tab.Screen name="Cafe" component={Cafe} options={{ headerShown: false }} />
-         <Tab.Screen name="CanaDeAcucar" component={CanaDeAcucar} options={{ headerShown: false }} />
-        <Tab.Screen name="Equatorial" component={Equatorial} options={{ headerShown: false }} />
-        <Tab.Screen name="TodasNoticias" component={TodasNoticias} options={{ headerShown: false }} />
-        <Tab.Screen name="TodasCotacao" component={TodasCotacao} options={{ headerShown: false }} />
+        <Tab.Screen
+          name="Inicio"
+          component={Inicio}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Buscar"
+          component={Buscar}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Cadastrar"
+          component={Cadastrar}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Conta"
+          component={Conta}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Perfil"
+          component={Perfil}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Plantas"
+          component={Plantas}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Climas"
+          component={Climas}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Milho"
+          component={Milho}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Soja"
+          component={Soja}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Cafe"
+          component={Cafe}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="CanaDeAcucar"
+          component={CanaDeAcucar}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Mandioca"
+          component={Mandioca}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Equatorial"
+          component={Equatorial}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="TodasNoticias"
+          component={TodasNoticias}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="TodasCotacao"
+          component={TodasCotacao}
+          options={{ headerShown: false }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
