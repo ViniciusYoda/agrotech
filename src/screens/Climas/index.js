c56b2730d41870d8e9fb31fd5d7dcd35b997e979
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-26 09:53:51 -0300

    Update index.js

--- src/screens/Climas/index.js
+++ src/screens/Climas/index.js
@@ -1,28 +1,35 @@
 import Card from '../../components/Card/card';
 import TopGreen from '../../components/TopGreen';
-import { Container } from './styles';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #D3D3D3;
+`;
 
 export default function Climas() {
-
-
   return (
     <Container>
       <TopGreen text="Climas no Brasil" iconName="arrow-left" />
       <Card
         imageSource={require('../../assets/equatorial.png')}
         name="Clima Equatorial"
+        rota="Equatorial"
       />
       <Card
         imageSource={require('../../assets/tropical.png')}
         name="Clima Tropical"
+        rota="Tropical"
       />
       <Card
         imageSource={require('../../assets/subtropical.png')}
         name="Clima Subtropical"
+        rota="Subtropical"
       />
       <Card
         imageSource={require('../../assets/semiarido.png')}
         name="Clima Semiárido"
+        rota="Semiarido"
       />
     </Container>
   );
