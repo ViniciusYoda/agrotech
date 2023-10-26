c9b92fe541979e407614508cfb9cf6492e274728
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-26 09:54:40 -0300

    Create index.js

---
+++ src/screens/Semiarido/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Info from '../../components/Info';
+import TopGreen from '../../components/TopGreen';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #D3D3D3;
+`;
+
+export default function ClimaSemiArido() {
+  return (
+    <Container>
+      <TopGreen text="Clima Semiárido" iconName="times" />
+      <Info
+        imageSource={require('../../assets/ClimaSemiarido.jfif')}
+        name="Clima Semiárido"
+        description="O clima semiárido é caracterizado por chuvas escassas
+e temperaturas elevadas. Esse ambiente climático apresenta desafios
+para a agricultura, mas é adequado para culturas resistentes à seca."
+      />
+    </Container>
+  );
+}
