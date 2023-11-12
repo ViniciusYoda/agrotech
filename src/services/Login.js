01fd435e87e64755352705baa22245605e59bafc
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-11-12 14:16:20 -0300

    Update Login.js

--- src/services/Login.js
+++ src/services/Login.js
@@ -3,7 +3,6 @@ import api from './api';
 
 export async function LoginUsuario(email, senha) {
   try {
-    console.log(email, senha)
     const response = await api.post('login', { email, senha });
     return response.data
   } catch (error) {
